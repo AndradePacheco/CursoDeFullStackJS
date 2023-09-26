489029ae3bb68a75053620e4fae1e45c5d1f5e58
Andrade Pacheco <144369908+AndradePacheco@users.noreply.github.com>
2023-09-26 15:02:38 +0100

    Revert "Exercício sobre props e agrupamento de components"
    This reverts commit 99da9da6f93b90a0bab3dc7d2da6282c21eda333.

--- DominandoOReact/production/my-firt-app/src/App.js
+++ DominandoOReact/production/my-firt-app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+<<<<<<< HEAD
 import Routes from './routes';
+=======
+import HelloWorld from './Hello';
+>>>>>>> parent of 99da9da (Exercício sobre props e agrupamento de components)
 
 function App() {
   return (
     <div>
+<<<<<<< HEAD
       <Routes/>
+=======
+      <HelloWorld/>
+>>>>>>> parent of 99da9da (Exercício sobre props e agrupamento de components)
     </div>
   );
 }
