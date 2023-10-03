3782cdf0bc3d70bdcea80f5bc188b5cbabeb8bf6
Andrade Pacheco <144369908+AndradePacheco@users.noreply.github.com>
2023-10-03 19:10:33 +0100

    vários avanços no desenvolvimento backend

--- DominandoOReact/backend/to-do-list/app.js
+++ DominandoOReact/backend/to-do-list/app.js
@@ -1,27 +1,12 @@
 const express = require("express");
+const checkListRouter = require('./src/routes/checklist');
+require("./config/database");
+
 
 const app = express();
-
 app.use(express.json());
 
-const log = (req, res, next) => {
-    console.log(`Data: ${Date.now()}`);
-    next();
-}
-
-app.use(log);
-
-app.get('/', (req,res) => {
-    res.send('<h1>Minha lista de tarefas</h1>');
-})
-
-app.get("/json", (req,res) => {
-    console.log(req.body)
-    res.json({
-        title: 'Tarefa x',
-        done: true
-    })
-})
+app.use('/checklist',checkListRouter);
 
 app.listen(3000, () => {
     console.log("O servidor foi iniciado!");
