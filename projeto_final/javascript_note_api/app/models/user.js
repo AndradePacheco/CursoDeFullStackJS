788e365f12d2ab57e0b967576ab7a62c7556bfbb
Andrade Pacheco <144369908+AndradePacheco@users.noreply.github.com>
2023-10-10 18:51:24 +0100

    instalação e configuração do bcrypt
    usado para encriptar as passwords dos usuários antes de guardá-las no banco de dados

--- projeto_final/javascript_note_api/app/models/user.js
+++ projeto_final/javascript_note_api/app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = mongoose.Schema({
     name: String,
@@ -6,11 +7,18 @@ let userSchema = mongoose.Schema({
     password:{type: String, required: true},
     created_at: {type: Date, default: Date.now},
     updated_at: {type: Date, default: Date.now},
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+})
+
+userSchema.pre('save', function(next){
+    if(this.new || this.modified('password')){
+        bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+            if(err) next(err);
+            else{
+                this.password = hashedPassword;
+                next();
+            }
+        })
     }
 })
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('User', userSchema);
